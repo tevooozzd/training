5ad7fbce42df0576e6ddf24b012e44c0d55d554b
tevoshw <estettv155@gmail.com>
2025-01-08 01:15:46 -0300

    Create UsingVariables2.c

---
+++ C/UsingVariables2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+// In this case i understand how to use one or more INT variables and print them all together
+
+int main () {
+
+    int MyAge; 
+    int MyMomAge; 
+    int MyDadAge;
+
+    MyAge = 17; 
+    MyMomAge = 49; 
+    MyDadAge = 49;
+
+    printf("My age is = %i\nMy Dad age is = %i\nMy Mom age is = %i", MyAge, MyDadAge, MyMomAge);
+    return 0;
+}
