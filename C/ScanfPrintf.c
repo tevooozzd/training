8ebb561be32a2cbd3d7a66785ce099ae0972da52
tevoshw <estettv155@gmail.com>
2025-01-12 20:13:40 -0300

    Create ScanfPrintf.c

---
+++ C/ScanfPrintf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// In this case i use the scanf and printf together to work on a more advanced model
+
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);  // Reads the user's age
+
+    printf("Your age is: %d\n", age);  // Display age on screen
+    return 0;
+}
