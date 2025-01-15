bb022f1c4c414bbeb1794c7d76f96ffb7036ae10
tevoshw <estettv155@gmail.com>
2025-01-14 23:54:13 -0300

    Create FUNCTIONS.c

---
+++ C/FUNCTIONS.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int sum(int a, int b){
+    return a + b;
+}
+
+
+int main() {
+
+    int result =  sum(2,3);
+    printf("the result of the sums gave x %d: ", result);
+    return 0;
+}
