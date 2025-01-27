b6ab0bdb52e58f0d255cdaeb41f13981cd9da155
tevooozzd <estettv155@gmail.com>
2025-01-27 02:08:18 -0300

    Create ARRAYS12.c

---
+++ C/ARRAYS12.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define tamanho 3
+
+
+int main(){
+    int arr[3][3];
+    int arrt[3][3];
+
+
+    printf("Insira os valores do array\n");
+    for (int i = 0; i < tamanho; i++)
+    {
+        for (int j = 0; j < tamanho; j++)
+        {
+            printf("Array %d %d receberá o valor: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+        
+        
+    }
+    
+    for (int i = 0; i < tamanho ; i++)
+    {
+        for (int j = 0; j < tamanho; j++)
+        {
+            arrt[i][j] = arr[j][i];
+        }
+        
+    }
+    
+    printf("Antes da trasposta: \n\n");
+    for (int i = 0; i < tamanho; i++)
+    {
+        for (int j = 0; j < tamanho; j++)
+        {
+            printf("%d", arr[i][j]);
+        }
+        printf("\n\n");
+    }
+    
+    
+    printf("Depois da traspota\n\n");
+    for (int i = 0; i < tamanho; i++)
+    {
+        for (int j = 0; j < tamanho; j++)
+        {
+            printf("%d", arrt[i][j]);
+        }
+        printf("\n\n");
+    }
+
+
+
+
+
+    return 0;
+}
